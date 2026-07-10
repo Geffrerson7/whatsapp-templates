6d9babd2b44cc67273aeaa7c42468ebc36da470e
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-07-09 19:51:44 -0500

    feat: load templates from localStorage on startup

--- js/persistence.js
+++ js/persistence.js
@@ -3,3 +3,8 @@ const CLAVE = "whatsapp-templates";
 function guardar() {
   localStorage.setItem(CLAVE, JSON.stringify(state.plantillas));
 }
+
+function cargar() {
+  const guardado = localStorage.getItem(CLAVE);
+  return guardado ? JSON.parse(guardado) : [];
+}
